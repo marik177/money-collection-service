224a8fca9449f0fffb1a0747b6546d94d7912f4c
Roman K <marik177@gmail.com>
2024-04-20 13:33:58 +0300

    create users

--- collection_project/users/apis.py
+++ collection_project/users/apis.py
@@ -1,9 +1,9 @@
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from collection_project.users.models import BaseUser
-from collection_project.users.services import user_list
+from collection_project.users.services import user_create, user_list
 
 
 # TODO: When JWT is resolved, add authenticated version
@@ -15,7 +15,19 @@ class UserListApi(APIView):
 
     def get(self, request):
         users = user_list()
-
         data = self.OutputSerializer(users, many=True).data
-
+        print(request.query_params, "request.query_params")
         return Response(data)
+
+
+class UserCreateApi(APIView):
+    class InputSerializer(serializers.ModelSerializer):
+        class Meta:
+            model = BaseUser
+            fields = ("email",)
+
+    def post(self, request):
+        serializer = self.InputSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user_create(**serializer.validated_data)
+        return Response(status=status.HTTP_201_CREATED)
--- collection_project/users/urls.py
+++ collection_project/users/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
-from .apis import UserListApi
+from .apis import UserCreateApi, UserListApi
 
-urlpatterns = [path("", UserListApi.as_view(), name="list")]
+urlpatterns = [
+    path("", UserListApi.as_view(), name="list"),
+    path("create/", UserCreateApi.as_view(), name="create"),
+]
