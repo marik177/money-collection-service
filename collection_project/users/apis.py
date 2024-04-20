24cba71a1205f6b5b59b106f00360f131bf16966
Roman K <marik177@gmail.com>
2024-04-20 15:32:38 +0300

    custom pagination

--- collection_project/users/apis.py
+++ collection_project/users/apis.py
@@ -2,12 +2,19 @@ from rest_framework import serializers, status
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from collection_project.api.pagination import (
+    LimitOffsetPagination,
+    get_paginated_response,
+)
 from collection_project.users.models import BaseUser
 from collection_project.users.services import user_create, user_list
 
 
 # TODO: When JWT is resolved, add authenticated version
 class UserListApi(APIView):
+    class Pagination(LimitOffsetPagination):
+        default_limit = 1
+
     class OutputSerializer(serializers.ModelSerializer):
         class Meta:
             model = BaseUser
@@ -15,9 +22,13 @@ class UserListApi(APIView):
 
     def get(self, request):
         users = user_list()
-        data = self.OutputSerializer(users, many=True).data
-        print(request.query_params, "request.query_params")
-        return Response(data)
+        return get_paginated_response(
+            pagination_class=self.Pagination,
+            serializer_class=self.OutputSerializer,
+            queryset=users,
+            request=request,
+            view=self,
+        )
 
 
 class UserCreateApi(APIView):
