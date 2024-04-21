f0636d6d34f28c7bd61272a366a6764f40469d96
Roman K <marik177@gmail.com>
2024-04-21 18:27:39 +0300

    occasion urls

--- collection_project/money_collections/urls.py
+++ collection_project/money_collections/urls.py
@@ -1,1 +1,14 @@
-# Create your tests here.
+from django.urls import include, path
+
+from collection_project.money_collections.apis.occasions import OccasionCreateApi, OccasionListApi
+
+occasions_urlpatterns = [
+    path("", OccasionListApi.as_view(), name="list"),
+    path("create/", OccasionCreateApi.as_view(), name="create"),
+    # path("<int:occasion_id>/", OccasionDetailApi.as_view(), name="detail"),
+    # path("<int:occasion_id>/update/", OccasionUpdateApi.as_view(), name="update"),
+]
+
+urlpatterns = [
+    path("occasions/", include((occasions_urlpatterns, "occasions"))),
+]
